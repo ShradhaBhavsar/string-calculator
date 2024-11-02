c7a0c70c9d21779b737f63c89bf2ec35872b79ec
SradhaBhavsar <shradha.bhavsar@deeporion.com>
2024-11-02 18:07:01 +0530

    #Commit-2: Added Test cases for complex inputs

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -19,5 +19,13 @@ RSpec.describe StringCalculator do
     it 'returns the sum for any amount of numbers' do
         expect(StringCalculator.add("1,2,3,4")).to eq(10)
     end
+
+    it 'handles new lines between numbers' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
+    it 'supports different delimiters' do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
