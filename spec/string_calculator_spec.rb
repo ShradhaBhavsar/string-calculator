5dd20383f615fa16c2a9f30fa0270bb8e06e1794
SradhaBhavsar <shradha.bhavsar@deeporion.com>
2024-11-02 18:08:23 +0530

    #Commit-2: Added Test cases for exceptions inputs

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -27,5 +27,13 @@ RSpec.describe StringCalculator do
     it 'supports different delimiters' do
       expect(StringCalculator.add("//;\n1;2")).to eq(3)
     end
+
+    it 'raises an exception for negative numbers' do
+      expect { StringCalculator.add("1,-2,3") }.to raise_error("negative numbers not allowed: -2")
+    end
+
+    it 'shows all negative numbers in the exception message' do
+      expect { StringCalculator.add("1,-2,-3,4") }.to raise_error("negative numbers not allowed: -2, -3")
+    end
   end
 end
