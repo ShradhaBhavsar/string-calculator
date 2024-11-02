c5075abd9651812fb5d84e5f12f66270a52b3428
SradhaBhavsar <shradha.bhavsar@deeporion.com>
2024-11-02 21:40:40 +0530

    #Commit-4: Added logic for method string addition

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,19 @@
 class StringCalculator
   def self.add(numbers)
-    #TODO addition logic
+    return 0 if numbers.empty?
+
+    delimiter = ","
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2..])
+      numbers = parts[1]
+    end
+
+    tokens = numbers.split(/#{delimiter}|\n/)
+    negatives = tokens.map(&:to_i).select { |n| n < 0 }
+
+    raise "negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
+
+    tokens.map(&:to_i).sum
   end
 end
